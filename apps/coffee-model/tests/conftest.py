c0d66bb8412e38c1eba2afbaef485ac9f8455652
raortega_meli <rafael.ortega@mercadolibre.com.co>
2025-09-21 21:15:14 -0500

    arreglos para incluir el joblib en el paquete

--- apps/coffee-model/tests/conftest.py
+++ apps/coffee-model/tests/conftest.py
@@ -6,4 +6,4 @@ from model.processing.data_manager import load_dataset
 
 @pytest.fixture()
 def sample_input_data():
-    return load_dataset(file_name=config.app_config.test_data_file)
+    return load_dataset(file_name=config.app_config.data_file)
